2358ead42b674d4be29d3ad56879fb0b64c43e5c
Lochmann Klaus <q391057@sfeu.bmw.corp>
2018-07-17 14:36:48 +0200

    cpu

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.get('/', function (req, res) {
   } else {
 		int a = 0;
 	  for(int i = 0; i < 1000; i ++) {
-		for(int j = 0; j < 1000; j ++) {
+		for(int j = 0; j < 100; j ++) {
 			a += j - i;
 		}
 	  }
